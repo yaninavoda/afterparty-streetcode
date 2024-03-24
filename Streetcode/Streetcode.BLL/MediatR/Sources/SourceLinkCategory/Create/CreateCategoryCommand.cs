31c3dcca25c584ed9dda022d4f6865b318bbe497
Ianina Voda <yaninavoda@gmail.com>
2024-03-24 11:44:35 +0200

    add CreateCategoryCommand

---
+++ Streetcode/Streetcode.BLL/MediatR/Sources/SourceLinkCategory/Create/CreateCategoryCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Streetcode.BLL.MediatR.Sources.SourceLinkCategory.Create;
+
+using BLL.Dto.Sources;
+using DAL.Entities.Sources;
+using FluentResults;
+
+public record CreateCategoryCommand(SourceLinkCategoryDto Category) : IRequest<Result<SourceLinkCategory>>;
