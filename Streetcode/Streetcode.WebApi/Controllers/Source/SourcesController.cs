b24e7c875428615b326781c07c35ded8e01d5b82
Ianina Voda <yaninavoda@gmail.com>
2024-03-24 12:44:51 +0200

    add CreateCategory action to SourcesController

--- Streetcode/Streetcode.WebApi/Controllers/Source/SourcesController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Source/SourcesController.cs
@@ -3,6 +3,8 @@ using Streetcode.BLL.MediatR.Sources.SourceLink.GetCategoryById;
 using Streetcode.BLL.MediatR.Sources.SourceLink.GetCategoriesByStreetcodeId;
 using Streetcode.BLL.MediatR.Sources.SourceLinkCategory.GetAll;
 using Streetcode.BLL.MediatR.Sources.SourceLinkCategory.GetCategoryContentByStreetcodeId;
+using Streetcode.BLL.Dto.Sources;
+using Streetcode.BLL.MediatR.Sources.SourceLinkCategory.Create;
 
 namespace Streetcode.WebApi.Controllers.Source;
 
@@ -36,5 +38,11 @@ public class SourcesController : BaseApiController
     public async Task<IActionResult> GetCategoriesByStreetcodeId([FromRoute] int streetcodeId)
     {
         return HandleResult(await Mediator.Send(new GetCategoriesByStreetcodeIdQuery(streetcodeId)));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCategory([FromBody] SourceLinkCategoryDto category)
+    {
+        return HandleResult(await Mediator.Send(new CreateCategoryCommand(category)));
     }
 }
