e0452112d3fa24f2461e70a21909b5b2f4470be5
OlhaPivniuk <pivnyuk_olga@ukr.net>
2024-03-24 18:09:57 +0200

    Add DTO for creating a new fact and repling to the request of fact creating

---
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/TextContent/Fact/CreateFactRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.BLL.Dto.Streetcode.TextContent.Fact
+{
+    public sealed record CreateFactRequestDto(string Title, string FactContent, int ImageId, int StreetcodeId);
+}
