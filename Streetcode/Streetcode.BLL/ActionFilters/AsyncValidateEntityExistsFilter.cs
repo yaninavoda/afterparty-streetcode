ba92e3fd20c319d43abf3f548ad56cd8ecf293c9
HlibBondarev <Hlib.Bondarev@gmail.com>
2024-04-02 14:13:51 +0300

    1) Created interface IEntity and action filter AsyncValidateEntityExistsFilter

---
+++ Streetcode/Streetcode.BLL/ActionFilters/AsyncValidateEntityExistsFilter.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.Resources.Errors;
+using Streetcode.DAL.Contracts;
+using Streetcode.DAL.Entities.Streetcode.TextContent;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using System.Security.Principal;
+
+namespace Streetcode.BLL.ActionFilters;
+
+public class AsyncValidateEntityExistsFilter<T> : IAsyncActionFilter
+    where T : class, IEntity
+{
+    private readonly IEntityRepositoryBase<T> _repositoryBase;
+    private readonly ILoggerService _logger;
+    private readonly IMapper _mapper;
+    public AsyncValidateEntityExistsFilter(IEntityRepositoryBase<T> repositoryBase, ILoggerService logger, IMapper mapper)
+    {
+        _repositoryBase = repositoryBase;
+        _logger = logger;
+        _mapper = mapper;
+    }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        const int INEXISTEDID = -1;
+        int id = INEXISTEDID;
+
+        if (context.ActionArguments.ContainsKey("id"))
+        {
+            id = (int)context.ActionArguments["id"]!;
+        }
+        else
+        {
+            var args = context.ActionArguments.FirstOrDefault().Value;
+            var iEntity = _mapper.Map<T>(args);
+            if (args is not null && iEntity is not null)
+            {
+                id = iEntity.Id;
+            }
+        }
+
+        if (id == INEXISTEDID)
+        {
+            var errorMsg = string.Format(ErrorMessages.RequestDoesNotContainIdParameter, typeof(T).Name);
+            _logger.LogError(context, errorMsg);
+            context.Result = new BadRequestObjectResult(errorMsg);
+            return;
+        }
+
+        var entity = await _repositoryBase.GetFirstOrDefaultAsync(x => x.Id.Equals(id));
+
+        if (entity == null)
+        {
+            string errorMessageType = GetErrorMessage(context.HttpContext.Request.Method);
+            var errorMsg = string.Format(errorMessageType, typeof(T).Name, id);
+            var expectedMessage = string.Format(
+            ErrorMessages.EntityByIdNotFound,
+            nameof(Fact),
+            id);
+            _logger.LogError(context, errorMsg);
+            context.Result = new NotFoundObjectResult(errorMsg);
+            return;
+        }
+        else
+        {
+            context.HttpContext.Items.Add("entity", entity);
+        }
+
+        var result = await next();
+    }
+
+    private string GetErrorMessage(string requestType) => requestType switch
+    {
+        "GET" => ErrorMessages.EntityByIdNotFound,
+        "PUT" => ErrorMessages.UpdateFailed,
+        "DELETE" => ErrorMessages.DeleteFailed,
+        _ => "Not appropriate Request type",
+    };
+}
