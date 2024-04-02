d89227ec32cab6c6e5c641d0844f2180409163b4
Ianina Voda <yaninavoda@gmail.com>
2024-04-02 11:11:25 +0300

    add null checks and private methods for them, use resource errorMessages

--- Streetcode/Streetcode.BLL/MediatR/Sources/SourceLinkCategory/Create/CreateCategoryHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Sources/SourceLinkCategory/Create/CreateCategoryHandler.cs
@@ -5,6 +5,9 @@ using Streetcode.BLL.Dto.Sources;
 using Streetcode.BLL.Interfaces.Logging;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 using Streetcode.DAL.Entities.Sources;
+using Streetcode.BLL.Resources.Errors;
+using Streetcode.DAL.Entities.Media.Images;
+using Streetcode.DAL.Entities.Streetcode;
 
 namespace Streetcode.BLL.MediatR.Sources.SourceLinkCategory.Create;
 
@@ -21,9 +24,21 @@ public class CreateCategoryHandler : IRequestHandler<CreateCategoryCommand, Resu
         _logger = logger;
     }
 
-    public async Task<Result<SourceLinkCategoryDto>> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+    public async Task<Result<SourceLinkCategoryDto>> Handle(CreateCategoryCommand command, CancellationToken cancellationToken)
     {
-        var newCategory = _mapper.Map<DAL.Entities.Sources.SourceLinkCategory>(request.Category);
+        var request = command.Category;
+
+        if (!await ImageExistsAsync(request.ImageId))
+        {
+            return ImageNotFoundError(request);
+        }
+
+        if (!await StreetcodeExistsAsync(request.StreetcodeId))
+        {
+            return StreetcodeNotFoundError(request);
+        }
+
+        var newCategory = _mapper.Map<DAL.Entities.Sources.SourceLinkCategory>(request);
 
         _repositoryWrapper.SourceCategoryRepository.Create(newCategory);
 
@@ -31,9 +46,9 @@ public class CreateCategoryHandler : IRequestHandler<CreateCategoryCommand, Resu
 
         var streetcodeCategoryContent = new StreetcodeCategoryContent()
         {
-            StreetcodeId = request.Category.StreetcodeId,
+            StreetcodeId = request.StreetcodeId,
             SourceLinkCategoryId = newCategory.Id,
-            Text = request.Category.Text
+            Text = request.Text
         };
 
         newCategory.StreetcodeCategoryContents.Add(streetcodeCategoryContent);
@@ -48,10 +63,53 @@ public class CreateCategoryHandler : IRequestHandler<CreateCategoryCommand, Resu
         }
         else
         {
-            string errorMsg = "Failed to create category";
-            _logger.LogError(request, errorMsg);
-
-            return Result.Fail(new Error(errorMsg));
+            return FailedToCreateCategoryError(request);
         }
+    }
+
+    private Result<SourceLinkCategoryDto> FailedToCreateCategoryError(CreateCategoryRequestDto request)
+    {
+        string errorMsg = string.Format(
+                ErrorMessages.CreateFailed,
+                nameof(SourceLinkCategory));
+        _logger.LogError(request, errorMsg);
+
+        return Result.Fail(errorMsg);
+    }
+
+    private async Task<bool> StreetcodeExistsAsync(int streetcodeId)
+    {
+        var streetcode = await _repositoryWrapper.StreetcodeRepository
+            .GetFirstOrDefaultAsync(s => s.Id == streetcodeId);
+
+        return streetcode is not null;
+    }
+
+    private async Task<bool> ImageExistsAsync(int imageId)
+    {
+        var image = await _repositoryWrapper.ImageRepository
+            .GetFirstOrDefaultAsync(i => i.Id == imageId);
+
+        return image is not null;
+    }
+
+    private Result<SourceLinkCategoryDto> ImageNotFoundError(CreateCategoryRequestDto request)
+    {
+        string errorMsg = string.Format(
+            ErrorMessages.EntityByIdNotFound,
+            nameof(Image),
+            request.ImageId);
+        _logger.LogError(request, errorMsg);
+        return Result.Fail(errorMsg);
+    }
+
+    private Result<SourceLinkCategoryDto> StreetcodeNotFoundError(CreateCategoryRequestDto request)
+    {
+        string errorMsg = string.Format(
+            ErrorMessages.EntityByIdNotFound,
+            nameof(StreetcodeContent),
+            request.StreetcodeId);
+        _logger.LogError(request, errorMsg);
+        return Result.Fail(errorMsg);
     }
 }
