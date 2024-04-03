6c7dc06588c2967f851ae937e2a191d749240790
Nazar Marteniuk <nazar.marteniuk@gmail.com>
2024-04-03 21:48:54 +0200

    fix/79: fix

--- Streetcode/Streetcode.BLL/MediatR/Sources/SourceLinkCategory/UpdateCategoryContent/UpdateCategoryContentCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using FluentResults;
-using MediatR;
-using Streetcode.BLL.Dto.Sources;
-using Streetcode.BLL.DTO.Sources;
-
-namespace Streetcode.BLL.MediatR.Sources.SourceLinkCategory.Update;
-
-public record class UpdateCategoryContentCommand(CategoryContentUpdateDto CategoryContentUpdateDto) : IRequest<Result<StreetcodeCategoryContentDto>>;
