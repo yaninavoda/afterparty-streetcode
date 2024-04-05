70b256724506eff1ed1701607c762c0b7bfcaf16
OlhaPivniuk <pivnyuk_olga@ukr.net>
2024-04-05 23:53:35 +0300

    Add DeleteTimelineItemCommand, DeleteTimelineItemHandler, action to TimelineItemController

---
+++ Streetcode/Streetcode.BLL/MediatR/Timeline/TimelineItem/Delete/DeleteTimelineItemCommand.cs
@@ -0,0 +1,7 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace Streetcode.BLL.MediatR.Timeline.TimelineItem.Delete
+{
+    public record DeleteTimelineItemCommand(int Id) : IRequest<Result<Unit>>;
+}
--- Streetcode/Streetcode.WebApi/Controllers/Timeline/TimelineItemController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Timeline/TimelineItemController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Streetcode.BLL.Dto.Timeline;
+using Streetcode.BLL.MediatR.Sources.SourceLinkCategory.Delete;
+using Streetcode.BLL.MediatR.Timeline.TimelineItem.Delete;
 using Streetcode.BLL.MediatR.Timeline.TimelineItem.GetAll;
 using Streetcode.BLL.MediatR.Timeline.TimelineItem.GetById;
 using Streetcode.BLL.MediatR.Timeline.TimelineItem.GetByStreetcodeId;
@@ -25,4 +27,11 @@ public class TimelineItemController : BaseApiController
     {
         return HandleResult(await Mediator.Send(new GetTimelineItemsByStreetcodeIdQuery(streetcodeId)));
     }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete([FromRoute] int id)
+    {
+        return HandleResult(await Mediator.Send(new DeleteTimelineItemCommand(id)));
+    }
+
 }
