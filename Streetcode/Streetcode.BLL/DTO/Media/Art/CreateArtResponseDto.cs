4f1d677456d8ccacf408597b32f9c11870f6d2b7
Ianina Voda <yaninavoda@gmail.com>
2024-04-06 17:08:54 +0300

    add request and response dtos

---
+++ Streetcode/Streetcode.BLL/DTO/Media/Art/CreateArtResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Streetcode.BLL.Dto.Media.Art;
+
+public class CreateArtResponseDto
+{
+    public int Id { get; set; }
+    public string? Description { get; set; }
+    public string? Title { get; set; }
+    public int ImageId { get; set; }
+    public int StreetcodeId { get; set; }
+}
