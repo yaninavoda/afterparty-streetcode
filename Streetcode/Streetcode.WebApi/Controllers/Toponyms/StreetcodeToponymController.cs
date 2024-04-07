90e369095a9b999b481b251dd615ca9b186c3f35
HlibBondarev <Hlib.Bondarev@gmail.com>
2024-04-07 14:02:53 +0300

    Replace [HttpDelete] under [HttpPost]

--- Streetcode/Streetcode.WebApi/Controllers/Toponyms/StreetcodeToponymController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Toponyms/StreetcodeToponymController.cs
@@ -7,15 +7,15 @@ namespace Streetcode.WebApi.Controllers.Toponyms;
 
 public class StreetcodeToponymController : BaseApiController
 {
-    [HttpDelete]
-    public async Task<IActionResult> Delete([FromBody] DeleteStreetcodeToponymRequestDto deleteRequest)
-    {
-        return HandleResult(await Mediator.Send(new DeleteStreetcodeToponymCommand(deleteRequest)));
-    }
-
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] CreateStreetcodeToponymRequestDto createRequest)
     {
         return HandleResult(await Mediator.Send(new CreateStreetcodeToponymCommand(createRequest)));
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete([FromBody] DeleteStreetcodeToponymRequestDto deleteRequest)
+    {
+        return HandleResult(await Mediator.Send(new DeleteStreetcodeToponymCommand(deleteRequest)));
     }
 }
