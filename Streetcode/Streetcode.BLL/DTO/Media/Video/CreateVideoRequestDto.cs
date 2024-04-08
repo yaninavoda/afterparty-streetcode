e474ad85f4ed699816b7adc0f81afc47f71eafb7
OlhaPivniuk <pivnyuk_olga@ukr.net>
2024-04-09 00:28:34 +0300

    Add Create logic for Video block

---
+++ Streetcode/Streetcode.BLL/DTO/Media/Video/CreateVideoRequestDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Streetcode.BLL.DTO.Media.Video
+{
+    public sealed record CreateVideoRequestDto(string? Description, string Url, int StreetcodeId);
+}
---
+++ Streetcode/Streetcode.BLL/MediatR/Media/Video/Create/CreateVideoRequestDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Streetcode.BLL.Dto.Media.Art;
+using Streetcode.BLL.DTO.Media.Video;
+
+namespace Streetcode.BLL.MediatR.Media.Video.Create
+{
+    public class CreateVideoRequestDtoValidator : AbstractValidator<CreateVideoRequestDto>
+    {
+        private const int MAXDESCRPTIONLENGTH = 400;
+
+        public CreateVideoRequestDtoValidator()
+        {
+            RuleFor(dto => dto.Description)
+                .MaximumLength(MAXDESCRPTIONLENGTH);
+
+            RuleFor(dto => dto.Url)
+                .Must(IsValidYoutubeUrl)
+                .WithMessage("Only youtube.com links are accepted.");
+
+            RuleFor(dto => dto.StreetcodeId)
+                .GreaterThan(0);
+        }
+
+        private bool IsValidYoutubeUrl(string url)
+        {
+            return url.Contains("youtube.com");
+        }
+    }
+}
