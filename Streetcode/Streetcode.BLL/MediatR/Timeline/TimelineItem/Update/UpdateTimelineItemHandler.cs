544a9f2857b0f969bfe5a093709c54f7b975fc72
Ianina Voda <yaninavoda@gmail.com>
2024-04-09 22:08:39 +0300

    all scenarios work

--- Streetcode/Streetcode.BLL/MediatR/Timeline/TimelineItem/Update/UpdateTimelineItemHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Timeline/TimelineItem/Update/UpdateTimelineItemHandler.cs
@@ -10,7 +10,7 @@ using Streetcode.DAL.Entities.Timeline;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 using HistoricalContextEntity = Streetcode.DAL.Entities.Timeline.HistoricalContext;
 using TimelineItemEntity = Streetcode.DAL.Entities.Timeline.TimelineItem;
-
+using HistoricalContextTimelineEntity = Streetcode.DAL.Entities.Timeline.HistoricalContextTimeline;
 namespace Streetcode.BLL.MediatR.Timeline.TimelineItem.Update;
 
 public class UpdateTimelineItemHandler : IRequestHandler<UpdateTimelineItemCommand, Result<TimelineItemDto>>
@@ -29,14 +29,86 @@ public class UpdateTimelineItemHandler : IRequestHandler<UpdateTimelineItemComma
     public async Task<Result<TimelineItemDto>> Handle(UpdateTimelineItemCommand command, CancellationToken cancellationToken)
     {
         var request = command.UpdateTimelineItemDto;
-
         TimelineItemEntity timelineItem = _mapper.Map<TimelineItemEntity>(request);
-
-        // TODO: deal with historical context
-        // if request.context is nullorempty - update and return ok
         TimelineItemDto responseDto;
-        if (string.IsNullOrEmpty(request.HistoricalContext))
+
+        if (!string.IsNullOrEmpty(request.HistoricalContext))
         {
+            HistoricalContextTimelineEntity historicalContextTimeline;
+
+            // find context by title  in db - if found check if there is a record in cross table, if not create a record;
+            HistoricalContextEntity? historicalContext = await _repositoryWrapper.HistoricalContextRepository
+                .GetFirstOrDefaultAsync(
+                hc => hc.Title == request.HistoricalContext,
+                h => h.Include(x => x.HistoricalContextTimelines));
+
+            if (historicalContext == null)
+            {
+                // create new historical context
+                historicalContext = new HistoricalContextEntity
+                {
+                    Title = request.HistoricalContext,
+                };
+
+                _repositoryWrapper.HistoricalContextRepository.Create(historicalContext);
+                await _repositoryWrapper.SaveChangesAsync();
+
+                // get its id, add to cross table
+                historicalContextTimeline = new ()
+                {
+                    TimelineId = request.Id,
+                    HistoricalContextId = historicalContext.Id,
+                };
+                _repositoryWrapper.HistoricalContextTimelineRepository.Create(historicalContextTimeline);
+            }
+            else
+            {
+                historicalContextTimeline = await _repositoryWrapper.HistoricalContextTimelineRepository.GetFirstOrDefaultAsync(
+                    hct => hct.TimelineId == request.Id && hct.HistoricalContextId == historicalContext.Id);
+                if (historicalContextTimeline == null)
+                {
+                    historicalContextTimeline = new ()
+                    {
+                        TimelineId = request.Id,
+                        HistoricalContextId = historicalContext.Id,
+                    };
+                    _repositoryWrapper.HistoricalContextTimelineRepository.Create(historicalContextTimeline);
+                }
+            }
+
+            _repositoryWrapper.TimelineRepository.Update(timelineItem!);
+
+            if (await _repositoryWrapper.SaveChangesAsync() <= 0)
+            {
+                return UpdateFailedError(request);
+            }
+
+            var updatedTimelineItem = await _repositoryWrapper.TimelineRepository
+            .GetFirstOrDefaultAsync(
+                predicate: ti => ti.Id == request.Id,
+                include: ti => ti
+                    .Include(til => til.HistoricalContextTimelines)
+                        .ThenInclude(x => x.HistoricalContext)!);
+
+            responseDto = _mapper.Map<TimelineItemDto>(updatedTimelineItem);
+
+            var historicalContextDtos = (await _repositoryWrapper.HistoricalContextTimelineRepository
+                .GetAllAsync(hct =>
+                hct.HistoricalContextId == historicalContextTimeline.HistoricalContextId
+                    && hct.TimelineId == historicalContextTimeline.TimelineId))
+                        .Select(pair => new HistoricalContextDto
+                        {
+                            Id = pair.HistoricalContextId,
+                            Title = request.HistoricalContext
+                        });
+
+            responseDto.HistoricalContexts = historicalContextDtos;
+
+            return Result.Ok(responseDto);
+        }
+        else
+        {
+            // if request.context is nullorempty - update and return ok
             _repositoryWrapper.TimelineRepository.Update(timelineItem!);
 
             if(await _repositoryWrapper.SaveChangesAsync() <= 0)
@@ -55,14 +127,6 @@ public class UpdateTimelineItemHandler : IRequestHandler<UpdateTimelineItemComma
 
             return Result.Ok(responseDto);
         }
-
-        // else find context by title  in db - if found check if there is a record in cross table, if not create a record;
-        // if not found - create new historical context, get its id, add to cross table
-
-        responseDto = _mapper.Map<TimelineItemDto>(timelineItem);
-
-        // add HistoricalContexts to dto
-        return Result.Ok(responseDto);
     }
 
     private Result<TimelineItemDto> UpdateFailedError(UpdateTimelineItemRequestDto request)
