04a16bb2041cf22e326df87f6d535a6f7e0058be
Ianina Voda <yaninavoda@gmail.com>
2024-04-13 16:41:19 +0300

    add register dto

---
+++ Streetcode/Streetcode.BLL/DTO/Account/RegisterUserDto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Streetcode.BLL.DTO.Account;
+
+public class RegisterUserDto
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+}
