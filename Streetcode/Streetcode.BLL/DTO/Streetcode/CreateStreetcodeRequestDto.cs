22da631562e6239fdd836399073c095d8b5f9fce
OlhaPivniuk <pivnyuk_olga@ukr.net>
2024-04-17 12:08:43 +0300

    Changed FirstName та LastName field to string? (Nullable)

--- Streetcode/Streetcode.BLL/DTO/Streetcode/CreateStreetcodeRequestDto.cs
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/CreateStreetcodeRequestDto.cs
@@ -7,9 +7,9 @@ namespace Streetcode.BLL.DTO.Streetcode
     {
         public StreetcodeType StreetcodeType { get; set; } = StreetcodeType.Person;
         public string Title { get; set; } = string.Empty;
-        public string FirstName { get; set; } = string.Empty;
+        public string? FirstName { get; set; }
         public string? Rank { get; init; }
-        public string LastName { get; set; } = string.Empty;
+        public string? LastName { get; set; }
         public DateTime EventStartOrPersonBirthDate { get; init; }
         public DateTime? EventEndOrPersonDeathDate { get; init; }
         public IEnumerable<int> TagIds { get; init; } = Array.Empty<int>();
