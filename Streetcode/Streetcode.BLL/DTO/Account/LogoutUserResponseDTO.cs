ad2c596bff9f1b0ad0668088f5a596e2997850cf
Nazar Marteniuk <nazar.marteniuk@gmail.com>
2024-04-19 16:59:13 +0200

    fix logout

---
+++ Streetcode/Streetcode.BLL/DTO/Account/LogoutUserResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.BLL.DTO.Account;
+
+public class LogoutUserResponseDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+}
